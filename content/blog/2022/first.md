6b7b912709afbd8c907a08175e6182075b207e93
Daniil V. <71486704+danvitu@users.noreply.github.com>
2024-08-24 16:02:44 +0300

    Initial git for MyNuxtPortfolio project

---
+++ content/blog/2022/first.md
@@ -0,0 +1,5 @@
+---
+publishedAt: 2022-07-12 17:16:00
+---
+
+# First post on out website in 2022
---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@nuxt/content",
+    "@nuxt/image",
+  ],
+  content: {
+    highlight: {
+      theme: {
+        default: "catppuccin-macchiato",
+      },
+    },
+  },
+  colorMode: {
+    classSuffix: "",
+  },
+  nitro: {
+    prerender: {
+      routes: ["/sitemap.xml"],
+    },
+  },
+});
