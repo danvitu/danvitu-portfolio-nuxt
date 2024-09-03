647787caa5ac1cbc76c7c1cd705cedd194d05dce
Daniil V. <71486704+danvitu@users.noreply.github.com>
2024-09-03 18:52:51 +0300

    fix social links

--- app/components/social-links.vue
+++ app/components/social-links.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="social-link">
+  <a href="https://github.com/danvitu" target="_blank" class="social-link">
     <Icon name="mdi:github" />
-    <a href="https://github.com/danvitu" target="_blank">GitHub</a>
-  </div>
-  <div class="social-link">
+    <p>GitHub</p>
+  </a>
+  <a href="https://t.me/danvitu" target="_blank" class="social-link">
     <Icon name="logos:telegram" />
-    <a href="https://t.me/danvitu" target="_blank">Telegram</a>
-  </div>
-  <div class="social-link">
+    <p>Telegram</p>
+  </a>
+  <a href="" class="social-link">
     <Icon name="logos:discord-icon" size="0.8rem" />
-    <a href="" target="_blank">Discord</a>
-  </div>
+    <p>Discord</p>
+  </a>
 </template>
 
 <style scoped>
 .social-link {
-  @apply flex gap-2 items-center w-max rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700 p-2
+  @apply flex gap-2 items-center rounded-lg hover:bg-gray-200 dark:hover:bg-gray-700 p-2
 }
 </style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,7 +3,7 @@
     <h1 class="mb-0">Привет 👋, меня зовут Даниил </h1>
     <p>Я разрабатываю веб-приложения и сайты на <strong>Vue / Nuxt</strong></p>
     <div
-      class="not-prose grid grid-cols-2 md:grid-cols-3 gap-y-4 rounded-2xl p-4 max-w-md bg-gray-50 dark:bg-gray-800 dark:text-white">
+      class="not-prose grid grid-cols-2 md:grid-cols-3 md:gap-4 rounded-2xl p-2 bg-gray-50 max-w-md dark:bg-gray-800 dark:text-white">
       <SocialLinks />
     </div>
     <h2>Избранные проекты</h2>
