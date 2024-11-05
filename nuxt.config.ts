d8c39ae8059fdf196e1cfcb9ed94c16e190bdb7e
Daniil V. <71486704+danvitu@users.noreply.github.com>
2024-11-05 22:46:53 +0300

    remove compatibility v4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  future: {
-    compatibilityVersion: 4,
-  },
   app: {
     pageTransition: { name: "page", mode: "out-in" },
   },
